d5f97b8f4456db442f54f98906882e01ce365196
canyon <123456@qq.com>
2023-02-03 14:06:35 +0800

    Reconciliation

--- main.js
+++ main.js
@@ -3,17 +3,23 @@
  * @Author: jianguo
  * @Date: 2023-01-30 20:49:56
  * @LastEditors: jianguo
- * @LastEditTime: 2023-02-01 11:48:10
+ * @LastEditTime: 2023-02-03 11:12:34
  */
 import { createElement,render } from "./micro-react";
 
 
-const element = createElement(
-  "h1",
-  { id:"title",class:"hello" },
-  "Hello Mirco-react",
-  createElement("h2")
-)
 const container = document.querySelector("#root")
-render(element,container)
-console.log(element);
+
+function handleInput(e){
+  renderer(e.target.value)
+}
+function renderer(value){
+  const element = createElement(
+  "div",
+  null,
+  createElement("input",{ oninput:(e)=>handleInput(e) },null),
+  createElement("h1",null,value)
+)
+  render(element,container)
+}
+renderer("hello")
--- micro-react/render.js
+++ micro-react/render.js
@@ -1,57 +1,113 @@
 
 function createDOM(fiber) {
     //创建元素
-    const dom = 
-        fiber.type === "TEXT_ELEMENT" 
-        ? document.createTextNode("") 
-        : document.createElement(fiber.type)
+    const dom =
+        fiber.type === "TEXT_ELEMENT"
+            ? document.createTextNode("")
+            : document.createElement(fiber.type)
     //附加属性
     Object.keys(fiber.props)
         .filter(key => key !== "children")
-            .map(name => dom[name] = fiber.props[name])
+        .map(name => dom[name] = fiber.props[name])
     //递归创建并渲染子节点（ 要渲染的子节点过多的话 会堵塞其他线程 故用调度函数进行渲染）
     // fiber.props.children.map(child => render(child,dom))
-    
+
     return dom
 }
 
-function render(element,container){
+function render(element, container) {
     wipRoot = {
-        dom:container,
-        props:{
-            children:[element]
-        }
+        dom: container,
+        props: {
+            children: [element]
+        },
+        alternate: currentRoot
     }
+    deletions = []
     nextUnitOfWork = wipRoot
 }
-function commitRoot(){
-    commitWrok( wipRoot.child )
+function commitRoot() {
+    deletions.map(commitWrok)
+    commitWrok(wipRoot.child)
+    currentRoot = wipRoot
     wipRoot = null
 }
-function commitWrok(fiber){
-    if( !fiber ){
-        return 
+
+function updateDOM(dom, prevProps, nextProps) {
+    // 删除老props
+    Object.keys( prevProps )
+    .filter(key => key!=="children")
+    .filter(key =>!(key in nextProps))
+    .forEach(key => 
+        dom[key] = ""
+    )
+    //删除旧事件
+    Object.keys( prevProps )
+    .filter(key=>key.startsWith("on"))
+    .filter(key => !(key in nextProps) || prevProps[key]!==nextProps[key])
+    .forEach(key=>{
+        const eventName = key.toLowerCase().substring(2) 
+        dom.removeEventListener( 
+            eventName,
+            prevProps[key]
+        )
+    })
+    //添加新事件
+    Object.keys( nextProps )
+    .filter(key =>key.startsWith("on"))
+    .filter(key => prevProps[key] !== nextProps[key])
+    .forEach(key =>{
+        const eventName = key.toLowerCase().substring(2)
+        dom.addEventListener(
+            eventName,
+            nextProps[key]
+        )
+    })
+    //添加新props
+    Object.keys( nextProps )
+    .filter(key => key!=="children")
+    .filter(key => !(key in prevProps) || prevProps[key]!==nextProps[key])
+    .forEach(key =>
+        dom[key] = nextProps[key]
+    )
+}
+function commitWrok(fiber) {
+    if (!fiber) {
+        return
     }
     const domParent = fiber.parent.dom
-    domParent.appendChild( fiber.dom )
-    commitWrok( fiber.child )
-    commitWrok( fiber.slibing )
+    if( fiber.effectTag == "PLACEMENT" && fiber.dom !== null){
+        domParent.appendChild(fiber.dom)
+    }else if( fiber.effectTag == "DELETION"){
+        domParent.removeChild(fiber.dom)
+    }else if( fiber.effectTag == "UPDATE" && fiber.dom !== null){
+        updateDOM(
+            fiber.dom,
+            fiber.alternate.props,
+            fiber.props
+        )
+    }
+    commitWrok(fiber.child)
+    commitWrok(fiber.sibling)
 }
 let nextUnitOfWork = null
+let currentRoot = null
 let wipRoot = null
+let deletions = null
+
 //调度函数 —— 空闲时进行渲染
-function workLoop(deadline){
+function workLoop(deadline) {
     //根据requestIdleCallback返回的deadline时间 确定是否允许渲染 默认为false
     let shouldYield = false
     //当存在渲染任务且有时间进行渲染时
-    while( nextUnitOfWork && !shouldYield ){
+    while (nextUnitOfWork && !shouldYield) {
         //performUnitOfWork 渲染传入任务并返回下一个渲染任务
         nextUnitOfWork = performUnitOfWork(nextUnitOfWork)
 
         shouldYield = deadline.timeRemaining() < 1
     }
     //没有下一个任务了 且具有初始渲染值 commit阶段
-    if( !nextUnitOfWork && wipRoot ){
+    if (!nextUnitOfWork && wipRoot) {
         commitRoot()
     }
     //如果未进行渲染 继续请求渲染 
@@ -59,52 +115,78 @@ function workLoop(deadline){
 }
 requestIdleCallback(workLoop)
 
-function performUnitOfWork( fiber ){
+function performUnitOfWork(fiber) {
     //创建DOM元素
-    if( !fiber.dom ){
-        fiber.dom = createDOM( fiber )
+    if (!fiber.dom) {
+        fiber.dom = createDOM(fiber)
     }
-    //如果有父节点就追加到父节点下————（渲染会被打断 用户可能会在浏览器上看到不完整的UI 故不在render执行）
-    // if( fiber.parent ){
-    //     fiber.parent.dom.appendChild(fiber.dom)
-    // }
     //给children新建fiber
     let elements = fiber.props.children
-    let index = 0
-    let perSlibing = null
-
-    while( index < elements.length ){
-        const element = elements[index]
-        
-        const newFiber = {
-            type: element.type,
-            props: element.props,
-            parent: fiber,
-            dom: null,
-            child: null,
-            slibing: null
-        }
-        //fiber只有一个child dom 剩余的是其上一个节点的slibing dom
-        if( index === 0){
-            fiber.child = newFiber
-        }else{
-            perSlibing.slibing = newFiber
-        }
-        perSlibing = newFiber
-        index ++
-    }
+    reconcileChildren(fiber, elements)
     //到这为止 完成当前任务了 并且要返回下一个任务
-    if( fiber.child ){
+    if (fiber.child) {
         return fiber.child
     }
     let nextFiber = fiber
-    while( nextFiber ){
-        if( nextFiber.slibing ){
-            return nextFiber.slibing
+    while (nextFiber) {
+        if (nextFiber.sibling) {
+            return nextFiber.sibling
         }
         nextFiber = nextFiber.parent
     }
 
+}
+//调和新旧节点
+function reconcileChildren(wipFiber, elements) {
+    let index = 0
+    let oldFiber = wipFiber.alternate && wipFiber.alternate.child
+    let perSibling = null
+
+    while (index < elements.length || oldFiber ) {
+        const element = elements[index]
+        const sameType = oldFiber && element && element.type === oldFiber.type
+        let newFiber = null;
+
+        //相同节点（属性值可能改变）
+        if( sameType ){
+            newFiber = {
+                type:oldFiber.type,
+                props:element.props,
+                parent:wipFiber,
+                dom:oldFiber.dom,
+                alternate:oldFiber,
+                effectTag: "UPDATE",
+            }
+        }
+        //新节点
+        if( element && !sameType ){
+            newFiber = {
+                type:element.type,
+                props:element.props,
+                parent:wipFiber,
+                dom:null,
+                alternate:null,
+                effectTag: "PLACEMENT",
+            }
+        }
+        //删除的节点
+        if( oldFiber && !sameType ){
+            oldFiber.effectTag = "DELETION"
+            deletions.push(oldFiber)
+        }
+        if( oldFiber ){
+            oldFiber = oldFiber.sibling
+        }
+
+        //fiber只有一个child dom 剩余的是其上一个节点的sibling dom
+        if (index == 0) {
+            wipFiber.child = newFiber
+        } else {
+            perSibling.sibling = newFiber
+        }
+        perSibling = newFiber
+        index++
+    }
 }
 
 export default render
